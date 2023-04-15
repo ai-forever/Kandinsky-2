ef85607207bc70ccbcbafb9c0101e5bb825e48e6
Chenxi <chenxi.whitehouse@gmail.com>
2023-04-15 12:47:39 +0000

    add more custom options

--- predict.py
+++ predict.py
@@ -1,7 +1,6 @@
+from typing import List
 from cog import BasePredictor, Input, Path
 from kandinsky2 import get_kandinsky2
-
-CACHE_DIR = "kandinsky2-weights"
 
 
 class Predictor(BasePredictor):
@@ -9,7 +8,7 @@ class Predictor(BasePredictor):
         self.model = get_kandinsky2(
             "cuda",
             task_type="text2img",
-            cache_dir="/tmp/kandinsky2",
+            cache_dir="./kandinsky2-weights",
             model_version="2.1",
             use_flash_attention=False,
         )
@@ -30,19 +29,37 @@ class Predictor(BasePredictor):
         ),
         prior_cf_scale: int = Input(default=4),
         prior_steps: str = Input(default="5"),
-    ) -> Path:
-        out = "/tmp/out.png"
-
+        width: int = Input(
+            description="Choose width. Lower the setting if out of memory.",
+            default=512,
+            choices=[256, 288, 432, 512, 576, 768, 1024],
+        ),
+        height: int = Input(
+            description="Choose height. Lower the setting if out of memory.",
+            default=512,
+            choices=[256, 288, 432, 512, 576, 768, 1024],
+        ),
+        batch_size: int = Input(
+            description="Choose batch size. Lower the setting if out of memory.",
+            default=1,
+            choices=[1, 2, 3, 4],
+        ),
+    ) -> List[Path]:
         images = self.model.generate_text2img(
             prompt,
             num_steps=num_inference_steps,
-            batch_size=1,
+            batch_size=batch_size,
             guidance_scale=guidance_scale,
-            h=768,
-            w=768,
+            h=height,
+            w=width,
             sampler=scheduler,
             prior_cf_scale=prior_cf_scale,
             prior_steps=prior_steps,
         )
-        images[0].save(out)
-        return Path(out)
+        output = []
+        for i, im in enumerate(images):
+            out = f"/tmp/out_{i}.png"
+            im.save(out)
+            im.save(f"out_{i}.png")
+            output.append(Path(out))
+        return output
