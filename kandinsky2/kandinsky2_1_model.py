ff943daecb6864565c451e0f5f60f8761bcae2b6
Michael Monashev <mmb@ya.ru>
2023-06-14 18:53:47 +0300

    Fix device for torch.load()

--- kandinsky2/kandinsky2_1_model.py
+++ kandinsky2/kandinsky2_1_model.py
@@ -54,7 +54,7 @@ class Kandinsky2_1:
             clip_mean,
             clip_std,
         )
-        self.prior.load_state_dict(torch.load(prior_path), strict=False)
+        self.prior.load_state_dict(torch.load(prior_path, map_location=self.device), strict=False)
         if self.use_fp16:
             self.prior = self.prior.half()
         self.text_encoder = TextEncoder(**self.config["text_enc_params"])
