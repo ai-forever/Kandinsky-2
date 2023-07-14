e75fb19c2b4d17c91c01f606c3ccd9d09a4e093c
Shahmatov Arseniy <62886550+cene555@users.noreply.github.com>
2023-07-14 13:29:04 +0300

    Update kandinsky2_2_model.py

--- kandinsky2/kandinsky2_2_model.py
+++ kandinsky2/kandinsky2_2_model.py
@@ -5,7 +5,7 @@ import math
 from copy import deepcopy
 import torch.nn.functional as F
 import numpy as np
-from diffusers import KandinskyV22Pipeline, KandinskyV22PriorPipeline, KandinskyV22Img2ImgPipeline
+from diffusers import KandinskyV22Pipeline, KandinskyV22PriorPipeline, KandinskyV22Img2ImgPipeline, KandinskyV22InpaintPipeline
 from transformers import CLIPVisionModelWithProjection
 from diffusers.models import UNet2DConditionModel
 from transformers import CLIPVisionModelWithProjection
@@ -27,6 +27,12 @@ class Kandinsky2_2:
             self.prior = KandinskyV22PriorPipeline.from_pretrained('kandinsky-community/kandinsky-2-2-prior', image_encoder=self.image_encoder, torch_dtype=torch.float16)
             self.prior = self.prior.to(self.device)
             self.decoder = KandinskyV22Pipeline.from_pretrained('kandinsky-community/kandinsky-2-2-decoder', unet=self.unet, torch_dtype=torch.float16)
+            self.decoder = self.decoder.to(self.device)
+        elif task_type == "inpainting":
+            self.unet = UNet2DConditionModel.from_pretrained('kandinsky-community/kandinsky-2-2-decoder-inpaint', subfolder='unet').to(torch.float16).to(self.device)
+            self.prior = KandinskyV22PriorPipeline.from_pretrained('kandinsky-community/kandinsky-2-2-prior', image_encoder=self.image_encoder, torch_dtype=torch.float16)
+            self.prior = self.prior.to(self.device)
+            self.decoder = KandinskyV22InpaintPipeline.from_pretrained('kandinsky-community/kandinsky-2-2-decoder-inpaint', unet=self.unet, torch_dtype=torch.float16)
             self.decoder = self.decoder.to(self.device)
         elif task_type == "img2img":
             self.unet = UNet2DConditionModel.from_pretrained('kandinsky-community/kandinsky-2-2-decoder', subfolder='unet').to(torch.float16).to(self.device)
@@ -50,7 +56,7 @@ class Kandinsky2_2:
         self,
         prompt,
         batch_size=1,
-        decoder_steps=100,
+        decoder_steps=50,
         prior_steps=25,
         decoder_guidance_scale=4,
         prior_guidance_scale=4,
@@ -110,7 +116,7 @@ class Kandinsky2_2:
         images_texts,
         weights,
         batch_size=1,
-        decoder_steps=100,
+        decoder_steps=50,
         prior_steps=25,
         decoder_guidance_scale=4,
         prior_guidance_scale=4,
@@ -130,8 +136,38 @@ class Kandinsky2_2:
             negative_emb = negative_emb.negative_image_embeds
         else:
             negative_emb = negative_emb.image_embeds
-        print(h, w)
         images = self.decoder(image_embeds=img_emb.image_embeds, negative_image_embeds=negative_emb,
                          num_inference_steps=decoder_steps, height=h,
                          width=w, guidance_scale=decoder_guidance_scale).images
+        return images
+
+    def generate_inpainting(
+        self,
+        prompt,
+        pil_img,
+        img_mask,
+        batch_size=1,
+        decoder_steps=50,
+        prior_steps=25,
+        decoder_guidance_scale=4,
+        prior_guidance_scale=4,
+        h=512,
+        w=512,
+        negative_prior_prompt="",
+        negative_decoder_prompt="",
+    ):
+        
+        img_emb = self.prior(prompt=prompt, num_inference_steps=prior_steps,
+                        num_images_per_prompt=batch_size, guidance_scale=prior_guidance_scale,
+                        negative_prompt=negative_prior_prompt)
+        negative_emb = self.prior(prompt=negative_prior_prompt, num_inference_steps=prior_steps,
+                             num_images_per_prompt=batch_size, guidance_scale=prior_guidance_scale)
+        if negative_decoder_prompt == "":
+            negative_emb = negative_emb.negative_image_embeds
+        else:
+            negative_emb = negative_emb.image_embeds
+        images = self.decoder(image_embeds=img_emb.image_embeds, negative_image_embeds=negative_emb,
+                         num_inference_steps=decoder_steps, height=h,
+                         width=w, guidance_scale=decoder_guidance_scale,
+                         image=pil_img, mask_image=img_mask).images
         return images
